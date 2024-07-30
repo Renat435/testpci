17eec6d78d8a57e57fe40a9d8f2b6264dbb4c18f
Ренат Парастаев <renatparastaev@MacBook-Air-Renat.local>
2024-07-30 13:50:32 +0300

    commit

--- main.js
+++ main.js
@@ -358,7 +358,7 @@ document.addEventListener("DOMContentLoaded", () => {
             inputNumber.value = localCardNumber.slice(0, 4) + ' ... ' + localCardNumber.slice(-4);
 
             if (!inputNumberValue.length) inputNumber.value = '';
-        } else if (!inputNumberValue.contains(' ... ')) {
+        } else if (!inputNumberValue.includes(' ... ')) {
             inputNumber.classList.add('error');
         }
     }
